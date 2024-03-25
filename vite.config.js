0bd390906e00a35dd4c86a43a2cbdaf949824c44
Arturo Espinosa Guadarrama <arturo.espinosa@qbit.com.mx>
2024-03-25 16:25:28 -0600

    Creación de proyecto y configuración de dependencias

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@mocks': path.resolve(__dirname, './src/mocks'),
+      '@reducers': path.resolve(__dirname, './src/reducers'),
+    }
+  },
+  plugins: [react()],
+})
